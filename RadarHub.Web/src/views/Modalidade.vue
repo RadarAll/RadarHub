2bb09546e524bc8a695385b504a6b2425d2a715f
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-11-16 18:48:09 -0300

    refactor: tentando melhorar table

--- RadarHub.Web/src/views/Modalidade.vue
+++ RadarHub.Web/src/views/Modalidade.vue
@@ -1,24 +1,47 @@
 <template>
     <v-app>
-        <v-navigation-drawer permanent :width="200" class="elevation-1">
+        <v-navigation-drawer permanent :width="250" class="elevation-1">
                 <div class="d-flex align-center justify-center w-full py-5">
                     <v-img :src="logo" :width="40" :height="40"/>
                 </div>
         </v-navigation-drawer>
         <v-app-bar class="elevation-1"></v-app-bar>
         <v-main>
-            <v-container class="ma-2">
-                <h1 class="text-green-primary">
-                    Modalidades de Licitações
-                </h1>
-                <v-data-table>
-                    <template #top>
-                        <v-toolbar color="green-light" flat>
-                            <v-toolbar-title class="">Licitações</v-toolbar-title>
-                        </v-toolbar>
-                    </template> 
-                </v-data-table>
-                
+            <v-container class="ma-2 mt-15">
+                <v-row>
+                    <v-col cols="8">
+                        <h1 class="text-green-primary"> Modalidades de Licitações </h1>
+                        <v-table class="border rounded elevation-2" density="comfortable">
+                            <thead>
+                                <tr>
+                                    <th class="text-h6 font-weight-bold">
+                                        Id
+                                    </th>
+                                    <th class="text-h6 font-weight-bold">
+                                        Nome
+                                    </th>
+                                    <th class="text-h6 font-weight-bold">
+                                        Ações
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    v-for="item in desserts"
+                                    :key="item.nome"
+                                >
+                                    <td class="text-body-1">{{ item.id }}</td>
+                                    <td class="text-body-1">{{ item.nome }}</td>
+                                </tr>
+                            </tbody>
+                        </v-table>
+                    </v-col>
+
+                    <v-col cols="4">
+                        <!-- Coluna para detalhes -->
+                    </v-col>
+
+                </v-row>
             </v-container>
         </v-main>
     </v-app>
@@ -28,17 +51,13 @@
 <script setup>
 import logo from '@/assets/logo.png'
 
-const headers = [
-  { title: 'ID', key: 'id' },
-  { title: 'Órgão', key: 'orgao' },
-  { title: 'Valor', key: 'valor' },
-  { title: 'Data', key: 'data' },
-]
-
-const items = [
-  { id: 1, orgao: 'Prefeitura de Marília', valor: 'R$ 120.000', data: '10/11/2025' },
-  { id: 2, orgao: 'Governo SP', valor: 'R$ 250.000', data: '12/11/2025' },
-  { id: 3, orgao: 'Ministério da Saúde', valor: 'R$ 1.000.000', data: '15/11/2025' },
+const desserts = [
+  { id: 1, nome: 'Concorrência Pública' },
+  { id: 2, nome: 'Tomada de Preços' },
+  { id: 3, nome: 'Convite' },
+  { id: 4, nome: 'Leilão' },
+  { id: 5, nome: 'Pregão Eletrônico' },
+  { id: 6, nome: 'RDC Integrado' },
 ]
 
 </script>
