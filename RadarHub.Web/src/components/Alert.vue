c230309439305f99ea02b33f5b01d4cd41fef73a
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-11-21 20:14:38 -0300

    feat: adicionado componente Alert

---
+++ RadarHub.Web/src/components/Alert.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row class="d-flex justify-center pt-12">
+
+    <!-- Sucesso -->
+    <div v-if="visivel && tipo === 'success'" class="w-25">
+      <v-alert type="success" border="start" variant="tonal" class="d-flex rounded-md elevation-2 justify-center font-weight-bold text-success bg-green-light">
+        {{ texto }}
+      </v-alert>
+    </div>
+
+    <!-- Loading -->
+    <div v-if="loading" class="w-25">
+      <v-alert type="loading" border="start" variant="tonal" class="d-flex rounded-md elevation-2 justify-center font-weight-bold text-green-primary bg-green-light">
+        Aguardando importação...
+      </v-alert>
+    </div>
+
+    <!-- Erro -->
+    <div v-if="visivel && tipo === 'error'" class="w-25">
+      <v-alert type="error"border="start" variant="tonal" class="d-flex rounded-md elevation-2 justify-center font-weight-bold text-red-primary bg-red-light">
+        {{ texto }}
+      </v-alert>
+    </div>
+
+  </v-row>
+</template>
+
+<script setup>
+
+defineProps({
+  visivel: { type: Boolean, default: false },
+  tipo: { type: String, default: 'info' }, // success | error | info
+  texto: { type: String, default: '' },
+  loading: { type: Boolean, default: false }
+})
+
+</script>
